a68f57ee3daf0b232ea268096c3d787432131fa0
Eric Xia <ericpingxia@gmail.com>
2022-06-24 19:02:22 -0700

    added rooms

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, join_room, leave_room
 from flask import Flask, render_template
 from application import create_app
-from application import routes
+
 
 app = create_app()
 app.config['SECRET_KEY'] = 'secret'
@@ -17,6 +17,21 @@ def index():
 def handleMessage(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
 
 if __name__ == '__main__':
     socketio.run(app)
