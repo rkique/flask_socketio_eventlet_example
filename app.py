95e3acdf5db7d278a31a3c8bd153e186ac194234
Eric Xia <ericpingxia@gmail.com>
2022-06-29 10:49:12 -0700

    correct deployment to Eventlet through starting app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask_socketio import SocketIO, send, join_room, leave_room
 from flask import Flask, render_template
 from application import create_app
+import eventlet
+import logging
 
 
 app = create_app()
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
+assignments = {}
 #routes
 @app.route('/')
 def index():
@@ -21,9 +24,20 @@ def handleMessage(msg):
 @socketio.on('join')
 def on_join(data):
     username = data['username']
+    #compare against assignments before this point
+    #if username already in a room, reject
+    print(assignments)
     room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', to=room)
+    if (username in assignments.keys()):
+        print("You are in a room.")
+        if (assignments[username] == room):
+            print("You're already in this room!")
+        else:
+          print("Leave your current room first before joining this one.")      
+    else: 
+        assignments[username] = room
+        join_room(room)
+        send(username + ' has entered the room.', to=room)
 
 @socketio.on('leave')
 def on_leave(data):
@@ -31,7 +45,13 @@ def on_leave(data):
     room = data['room']
     leave_room(room)
     send(username + ' has left the room.', to=room)
+    if (username in assignments.keys()):
+        if (assignments[username] == room):
+            print("Left the current room.")
+            del assignments[username]
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    #FLASK RUN does NOT call APP.PY...
+    logging.basicConfig(level=logging.ERROR)
+    socketio.run(app, port=8000, log_output=False, debug=False)
--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 rem this is a quickstart script
 set FLASK_APP=app.py
-set FLASK_ENV=development
+set FLASK_ENV=production
 start "" http://localhost:8000/
-flask run --port=8000
+py app.py
 cmd /k
 
